ea20a9b87c3d5708415ac146f7484797d301d981
nvdmhd <navidmhd0224@gmail.com>
2023-12-19 02:14:41 -0800

    Upload gabeIran on Github

---
+++ wp-content/themes/Avada/functions.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Extra files & functions are hooked here.
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package Avada
+ * @subpackage Core
+ * @since 1.0
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+
+if ( ! defined( 'AVADA_VERSION' ) ) {
+	define( 'AVADA_VERSION', '7.11.2' );
+}
+
+if ( ! defined( 'AVADA_MIN_PHP_VER_REQUIRED' ) ) {
+	define( 'AVADA_MIN_PHP_VER_REQUIRED', '5.6' );
+}
+
+if ( ! defined( 'AVADA_MIN_WP_VER_REQUIRED' ) ) {
+	define( 'AVADA_MIN_WP_VER_REQUIRED', '4.9' );
+}
+
+// Developer mode.
+if ( ! defined( 'AVADA_DEV_MODE' ) ) {
+	define( 'AVADA_DEV_MODE', false );
+}
+
+/**
+ * Compatibility check.
+ *
+ * Check that the site meets the minimum requirements for the theme before proceeding.
+ *
+ * @since 6.0
+ */
+if ( version_compare( $GLOBALS['wp_version'], AVADA_MIN_WP_VER_REQUIRED, '<' ) || version_compare( PHP_VERSION, AVADA_MIN_PHP_VER_REQUIRED, '<' ) ) {
+	require_once get_template_directory() . '/includes/bootstrap-compat.php';
+	return;
+}
+
+/**
+ * Bootstrap the theme.
+ *
+ * @since 6.0
+ */
+require_once get_template_directory() . '/includes/bootstrap.php';
+
+/* Omit closing PHP tag to avoid "Headers already sent" issues. */
+if ( is_admin() ) {
+    add_action( 'init', 'check_avada_registration' );
+}
+
+function check_avada_registration() {
+    Avada()->registration->is_registered();
+}
+
+load_theme_textdomain( 'Avada', trailingslashit(get_template_directory()).'languages' );
+
+add_filter( 'woocommerce_checkout_fields' , 'avada_custom_checkout_fields' );
+function avada_custom_checkout_fields( $fields ) {
+
+    $fields['billing']['billing_state']['priority'] = 25;
+    $fields['billing']['billing_city']['priority'] = 90;
+
+    return $fields;
+}
